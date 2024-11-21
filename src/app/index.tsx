4936dd06844ee00c651c482da444c25750eebc4d
Leah Keenwood <60515000+soleilfenley@users.noreply.github.com>
2024-11-21 16:16:49 -0600

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl font-bold">The start of something beautiful.</Text>
+    </View>
+  );
+}
